3944f9a52026c967b52b4c973e5b8c044c1208a9
monke181 <ko.engineering181@gmail.com>
2025-12-30 14:51:53 -0800

    usaco cow signal

---
+++ USACO Coding/usaco python/usacoCowSignal.py
@@ -0,0 +1,15 @@
+with open('usaco python/xInput/cowsignal.in', 'r') as read:
+    M, N, K = map(int, read.readline().split())
+
+    original_signal = []
+
+    for _ in range(N):
+        line = read.readline().strip() 
+        original_signal.append(line)
+with open('cowsignal.out', 'w') as out:
+    for row in original_signal:
+        scaled_row = ""
+        for i in row:
+            scaled_row += i * K
+        for _ in range(K):
+            print(scaled_row, file=out)
